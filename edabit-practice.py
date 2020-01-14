7560dab1fbb83860daa8738782048fa6a13b2fe8
Roger Mayer <rmayer1984@gmail.com>
2020-01-14 14:56:41 -0600

    completed to_str to_int ex edabit

--- edabit-practice.py
+++ edabit-practice.py
@@ -17,7 +17,25 @@ print(hello())
 # return area of triangle
 def tri_area(base, height):
     area = (base * height) / 2
+
     return area
 
 
 print(tri_area(3, 2))
+
+
+# to_int() : A function to convert a string to an integer.
+# to_str() : A function to convert an integer to a string.
+
+def to_int(txt):
+    return int(txt)
+
+
+print(to_int('123'))
+
+
+def to_str(num):
+    return str(num)
+
+
+print(to_str(123))
