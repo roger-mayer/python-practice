986d6933e59c30bba2224b714ea3f5a10810a5d7
Roger Mayer <rmayer1984@gmail.com>
2020-01-14 14:35:01 -0600

    starting edabit python practice problems

---
+++ edabit-practice.py
@@ -0,0 +1,23 @@
+# Return the sum of two numbers
+def addition(a, b):
+    return a + b
+
+
+print("3 + 5 =", addition(3, 2))
+
+
+# Return hello message in hello() function
+def hello():
+    return "hi roger"
+
+
+print(hello())
+
+
+# return area of triangle
+def tri_area(base, height):
+    area = (base * height) / 2
+    return area
+
+
+print tri_area(3, 2)
