835d1c99f2fdd88d2426bbcb70acc5ee8ba697cd
Roger Mayer <rmayer1984@gmail.com>
2020-01-19 22:22:35 -0600

    completed string review

---
+++ crash_course/personal_message.py
@@ -0,0 +1,13 @@
+name = "roger"
+print("Hello " + name)
+
+print("Hello " + name.title())
+print("Hello " + name.upper())
+name = name.upper()
+print(name)
+print("Hello " + name.lower())
+
+print('Einstein once said, "It\'s all relative\"')
+name = "Einstein"
+print(name + " once said, \"It\'s all relative\"")
+
