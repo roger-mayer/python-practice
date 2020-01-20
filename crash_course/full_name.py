86c47d5346ba63f63bf292cd280d680f0b694b78
Roger Mayer <rmayer1984@gmail.com>
2020-01-19 21:57:37 -0600

    added note

--- crash_course/full_name.py
+++ crash_course/full_name.py
@@ -1,4 +1,3 @@
-
 first_name = "roger"
 last_name = "mayer"
 
@@ -8,19 +7,20 @@ full_name = f"{first_name} {last_name}"
 print(full_name)
 print(f"Hello, {full_name.title()}!")
 
-
 # adding whitespace
 
 print("python")
 print("\tPython")  # adds tab to printed line
 print("Languages: \nPython\nCSS\nJavaScript")  # new line
 
-print("Languages: \n\tPython\n\tCSS\n\tJavaScript") # new line
+print("Languages: \n\tPython\n\tCSS\n\tJavaScript")  # new line
 
 # removing whitespace
 
 extra_space = "hello   "
 print(extra_space, "|")
-extra_space = extra_space.rstrip()
+extra_space = extra_space.strip()
 print(extra_space, "|")
+
+# also .rstrip and .lstrip
 
