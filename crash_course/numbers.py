cd6073b1fce55c8da67ecde0be982386d627c54e
Roger Mayer <rmayer1984@gmail.com>
2020-01-19 22:35:08 -0600

    working through numbers ex

---
+++ crash_course/numbers.py
@@ -0,0 +1,21 @@
+# integers
+
+print(2 + 3)  # 5
+print(3 - 2)  # 1
+print(2 * 3)  # 3
+print(3 / 2)  # 1.5
+
+print(3 ** 2)  # 9 (exponent)
+print(2 + 3 * 4)  # 14 (order of operations
+print((2 + 3) * 4)  # 20
+
+# floats
+
+print(.1 + .1)  # .2
+
+print(.2 + .1)  # 0.30000000000000004
+print(3 * .01)  # 0.30000000000000004
+
+
+
+
