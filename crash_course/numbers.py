0d6c5f6419024decee0137d936d6ec3cdcb9fb23
Roger Mayer <rmayer1984@gmail.com>
2020-01-26 10:17:00 -0600

    completed numbers ex

--- crash_course/numbers.py
+++ crash_course/numbers.py
@@ -12,12 +12,19 @@ print((2 + 3) * 4)  # 20
 # floats
 
 print(.1 + .1)  # .2
-
 print(.2 + .1)  # 0.30000000000000004
 print(3 * .01)  # 0.30000000000000004
 
+# underscores in numbers
 
+universe_age = 14_000_000_000
+print(universe_age)
 
+# multiple assignments
 
+x, y, z = 1, 2, 3
+print(x, y, z)
 
-
+# capital letters treated as constants
+STAY_FOREVER = 100
+print(STAY_FOREVER)
