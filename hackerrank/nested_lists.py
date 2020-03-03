bbc9995e68cee1435fbb3af0f9ff7c348ad6a003
Roger Mayer <rmayer1984@gmail.com>
2020-03-03 11:44:46 -0600

    added instructions

--- hackerrank/nested_lists.py
+++ hackerrank/nested_lists.py
@@ -2,6 +2,13 @@
 # students, store them in a nested list and print the name(s) of any student(s)
 # having the second lowest grade.
 
+# There are  students in this class whose names and grades are assembled to build the following list:
+
+# python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+
+# The lowest grade of  37.2 belongs to Tina. The second lowest grade of 37.21 belongs to both Harry and Berry,
+# so we order their names alphabetically and print each name on a new line.
+
 
 marksheet = []
 scorelist = []
