34b58dbcb87778e78475892bb4efa99a4cb60845
Roger Mayer <rmayer1984@gmail.com>
2020-03-04 16:28:48 -0600

    started while loops ex

---
+++ crash_course/input_and_while_loops/whiles.py
@@ -0,0 +1,14 @@
+num = 1
+while num <= 5:
+    print(num)
+    num += 1
+
+prompt = "\nTell me something and I will repeat it back"
+prompt += "\nenter 'exit' to end program: "
+
+message = ""
+while message != 'exit':
+    message = input(prompt)
+    if message != 'exit':
+        print(message)
+
