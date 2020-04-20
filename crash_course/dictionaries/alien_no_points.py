c69df5f323d10514362a136fd1baf2d3bc514066
Roger Mayer <rmayer1984@gmail.com>
2020-04-19 22:26:40 -0500

    completed get() ex

--- crash_course/dictionaries/alien_no_points.py
+++ crash_course/dictionaries/alien_no_points.py
@@ -1,1 +1,5 @@
-# using get to access values
+# using get() to access values
+alien_0 = {'color': 'green', 'speed': 'slow'}
+point_value = alien_0.get('points', 'no value assigned')
+
+print(point_value)
