b6fa49de6f2a8605e7748dabbde31f559af3c10f
Roger Mayer <rmayer1984@gmail.com>
2020-06-29 09:55:58 -0500

    added passing info function

--- crash_course/functions/greeter.py
+++ crash_course/functions/greeter.py
@@ -1,5 +1,14 @@
+# defining a function
 def greet_user():
     print("hello")
 
 
 greet_user()
+
+
+# passing info to a function
+def greet_name(username):
+    print(f"Hello, {username.title()}!")
+
+
+greet_name('roger')
