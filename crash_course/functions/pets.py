b275938a6c5b28ef29a75fc9c422ba7316c25496
Roger Mayer <rmayer1984@gmail.com>
2020-07-02 15:08:30 -0500

    added default value to pets

--- crash_course/functions/pets.py
+++ crash_course/functions/pets.py
@@ -1,10 +1,7 @@
-def describe_pet(animal_type, pet_name):
+def describe_pet(pet_name, animal_type='fish'):
     print(f"\nI have a {animal_type}.")
     print(f"\nMy {animal_type}'s name is {pet_name.title()}.")
 
 
 describe_pet('dog', 'sky')
 describe_pet('cat', 'waffles')
-
-
-
